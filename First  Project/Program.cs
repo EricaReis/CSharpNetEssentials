06103c5b6cf4d2d30faa3eba202015e22d043b72
EricaReis <ericareis67@outlook.com>
2025-03-26 20:56:09 -0300

    Learning basic syntax

--- First  Project/Program.cs
+++ First  Project/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Aula01;
+﻿namespace BasicSyntax;
 
 public class Program
 {
     public static void Main ()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Type a number:");
+
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int result = number % 2;
+
+        if (result == 0) 
+        {
+            Console.WriteLine("Even");
+        } else
+        {
+            Console.WriteLine("Odd");
+        }
+
     }
 }
 
