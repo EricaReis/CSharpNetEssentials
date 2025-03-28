caf26995ceb76a6bc0fb66445f6967d884024990
EricaReis <ericareis67@outlook.com>
2025-03-28 20:35:35 -0300

    Assignment Operators

---
+++ AssignmentOperators/AssignmentOperators/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace AssignmentOperators;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Assignment Operators");
+
+        int x = 10;
+
+        Console.WriteLine("\n Initial Value: " + x);
+
+        //Simple Assignment operator (=):
+        x = 20;
+        Console.WriteLine("Simple assignment: " + x);
+
+        //Compound Assignment:
+        x++;
+        Console.WriteLine("Compound Assignment ++: " + x);
+
+
+        //Compound Assignment (x = x + 10):
+        x += 10;
+        Console.WriteLine("Compound Assignment +=: " + x);
+
+        //Compound Assignment (x = x - 10):
+        x -= 10;
+        Console.WriteLine("Compound Assignment -=: " + x);
+
+        //Compound Assignment (x = x * 10):
+        x *= 10;
+        Console.WriteLine("Compound Assignment *=: " + x);
+
+        //Compound Assignment (x = x / 10):
+        x /= 10;
+        Console.WriteLine("Compound Assignment /=: " + x);
+    }
+}
